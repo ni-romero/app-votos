862ef300f51d4d4379f9356d2a34191ef914dc54
ni-romero <luz.nicole.maria@gmail.com>
2020-12-04 11:30:34 -0300

    tamaño icono2

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import {Button} from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import { useHistory } from "react-router-dom";
 // import { Link } from 'react-router-dom';
 import './App.css';
 
-const Inicio = () =>{
+const Inicio = () => {
     let history = useHistory();
-    return(
+    return (
         <>
-        <div className="padreInicio">
-        
-            <div className="m-auto p-4">
-            <h1 className=" text-white text-center "> Fuerza Fiscal</h1>
-           <div className="content-vacio p-4">
-               </div> 
-            <br></br>
-          <Button className="btn-inicio1 my-2 form-control"
-           onClick={() => history.push("/register")} >Registrarse</Button>  <br></br>
-          <Button  className="btn-inicio2 my-3 form-control"
-           onClick={() => history.push("/inicio-sesion")} > Iniciar sesión</Button>
-          </div>
-          </div>
-         </>
+            <div className="padreInicio">
+
+                <div className="p-4">
+                    <br></br>
+                    <h1 className=" text-white text-center "> Fuerza Fiscal</h1>
+                    <br></br>  <br></br>
+                    <div className="content-vacio p-4">
+                    </div>
+                    <br></br>
+                    <Button className="btn-inicio1 my-2 form-control"
+                        onClick={() => history.push("/register")} >Registrarse</Button>  <br></br>
+                    <Button className="btn-inicio2 my-3 form-control"
+                        onClick={() => history.push("/inicio-sesion")} > Iniciar sesión</Button>
+                </div>
+            </div>
+        </>
     )
 }
 
